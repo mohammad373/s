ace923b53549071bfc5993f904b6f683a26fa980
mohammad373 <75175976+mohammad373@users.noreply.github.com>
2020-12-07 00:09:44 +0330

    Update s.py

--- s.py
+++ s.py
@@ -10,7 +10,7 @@ def __name__():
 
         #print(Fore.BLUE + "Hello . Welcome Back" + Fore.YELLOW + " ;)")
         # m
-        target = input(Fore.RED + "Enter Your Address Target" + For.YELLOW + " ==>  ")
+        target = input(Fore.RED + "Enter Your Address Target" + Fore.YELLOW + " ==>  ")
         time.sleep(1.3)
         
         if target == "" or None :
